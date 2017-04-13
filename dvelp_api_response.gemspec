1fdbeb9cf0691a164e73a56afc9ac187aa7b4edf
Richard Brown <richard@dvelp.co.uk>
2017-04-13 15:51:32 +0100

    Setup rspec
    Why?
    - Enable testing of the gem
    
    How?
    - Add combustion to setup dummy app
    - Update spec_helper to require relevant gems and initialisations

--- dvelp_api_response.gemspec
+++ dvelp_api_response.gemspec
@@ -27,8 +27,15 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ['lib']
 
+  spec.add_dependency 'rails'
+
   spec.add_development_dependency 'bundler', '~> 1.14'
+  spec.add_development_dependency 'byebug'
+  spec.add_development_dependency 'combustion'
+  spec.add_development_dependency 'database_cleaner'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rubocop'
   spec.add_development_dependency 'rspec', '~> 3.0'
+  spec.add_development_dependency 'rspec-rails'
+  spec.add_development_dependency 'sqlite3'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,23 @@
 # frozen_string_literal: true
 
+ENV['RAILS_ENV'] ||= 'test'
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'dvelp_api_response'
 
+require 'combustion'
+Combustion.initialize! :all
+
+require 'byebug'
+require 'database_cleaner'
+require 'rspec/rails'
+
+# Load support files
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
 RSpec.configure do |config|
+  config.use_transactional_fixtures = true
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
@@ -13,4 +27,5 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.order = :random
 end
